f5b26d5ee88f5a2ff6fedd1354ad250a45675ca7
Theodore Aptekarev <aptekarev@gmail.com>
2022-02-09 18:24:10 +0300

    Fix external axes mode it cusum plot

--- detecta/detect_cusum.py
+++ detecta/detect_cusum.py
@@ -80,11 +80,11 @@ def detect_cusum(x, threshold=1, drift=0, ending=False, show=True, ax=None):
 
     >>> x = 2*np.sin(2*np.pi*np.arange(0, 3, .01))
     >>> ta, tai, taf, amp = detect_cusum(x, 1, .05, True, True)
- 
+
     Version history
     ---------------
     '1.0.5':
-        Part of the detecta module - https://pypi.org/project/detecta/   
+        Part of the detecta module - https://pypi.org/project/detecta/
     """
 
     x = np.atleast_1d(x).astype('float64')
@@ -145,39 +145,45 @@ def _plot(x, threshold, drift, ending, ax, ta, tai, taf, gp, gn):
         import matplotlib.pyplot as plt
     except ImportError:
         print('matplotlib is not available.')
-    else:
-        if ax is None:
-            _, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
+        return
 
-        t = range(x.size)
-        ax1.plot(t, x, 'b-', lw=2)
-        if len(ta):
-            ax1.plot(tai, x[tai], '>', mfc='g', mec='g', ms=10,
-                     label='Start')
-            if ending:
-                ax1.plot(taf, x[taf], '<', mfc='g', mec='g', ms=10,
-                         label='Ending')
-            ax1.plot(ta, x[ta], 'o', mfc='r', mec='r', mew=1, ms=5,
-                     label='Alarm')
-            ax1.legend(loc='best', framealpha=.5, numpoints=1)
-        ax1.set_xlim(-.01*x.size, x.size*1.01-1)
-        ax1.set_xlabel('Data #', fontsize=14)
-        ax1.set_ylabel('Amplitude', fontsize=14)
-        ymin, ymax = x[np.isfinite(x)].min(), x[np.isfinite(x)].max()
-        yrange = ymax - ymin if ymax > ymin else 1
-        ax1.set_ylim(ymin - 0.1*yrange, ymax + 0.1*yrange)
-        ax1.set_title('Time series and detected changes ' +
-                      '(threshold= %.3g, drift= %.3g): N changes = %d'
-                      % (threshold, drift, len(tai)))
-        ax2.plot(t, gp, 'y-', label='+')
-        ax2.plot(t, gn, 'm-', label='-')
-        ax2.set_xlim(-.01*x.size, x.size*1.01-1)
-        ax2.set_xlabel('Data #', fontsize=14)
-        ax2.set_ylim(-0.01*threshold, 1.1*threshold)
-        ax2.axhline(threshold, color='r')
-        ax1.set_ylabel('Amplitude', fontsize=14)
-        ax2.set_title('Time series of the cumulative sums of ' +
-                      'positive and negative changes')
-        ax2.legend(loc='best', framealpha=.5, numpoints=1)
-        plt.tight_layout()
-        plt.show()
+    if ax is None:
+        _, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
+    else:
+        if len(ax) != 2:
+            print("Plotting failed. Expecting instances of 2 matplotlib axes.")
+            return
+        (ax1, ax2) = ax
+
+    t = range(x.size)
+    ax1.plot(t, x, 'b-', lw=2)
+    if len(ta):
+        ax1.plot(tai, x[tai], '>', mfc='g', mec='g', ms=10,
+                    label='Start')
+        if ending:
+            ax1.plot(taf, x[taf], '<', mfc='g', mec='g', ms=10,
+                        label='Ending')
+        ax1.plot(ta, x[ta], 'o', mfc='r', mec='r', mew=1, ms=5,
+                    label='Alarm')
+        ax1.legend(loc='best', framealpha=.5, numpoints=1)
+    ax1.set_xlim(-.01*x.size, x.size*1.01-1)
+    ax1.set_xlabel('Data #', fontsize=14)
+    ax1.set_ylabel('Amplitude', fontsize=14)
+    ymin, ymax = x[np.isfinite(x)].min(), x[np.isfinite(x)].max()
+    yrange = ymax - ymin if ymax > ymin else 1
+    ax1.set_ylim(ymin - 0.1*yrange, ymax + 0.1*yrange)
+    ax1.set_title('Time series and detected changes ' +
+                    '(threshold= %.3g, drift= %.3g): N changes = %d'
+                    % (threshold, drift, len(tai)))
+    ax2.plot(t, gp, 'y-', label='+')
+    ax2.plot(t, gn, 'm-', label='-')
+    ax2.set_xlim(-.01*x.size, x.size*1.01-1)
+    ax2.set_xlabel('Data #', fontsize=14)
+    ax2.set_ylim(-0.01*threshold, 1.1*threshold)
+    ax2.axhline(threshold, color='r')
+    ax1.set_ylabel('Amplitude', fontsize=14)
+    ax2.set_title('Time series of the cumulative sums of ' +
+                    'positive and negative changes')
+    ax2.legend(loc='best', framealpha=.5, numpoints=1)
+    plt.tight_layout()
+    plt.show()
